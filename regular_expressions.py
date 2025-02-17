dc25921e9ca950701fb10453d3012526915550f0
rachek253 <128507881+rachek253@users.noreply.github.com>
2025-02-17 16:32:27 -0700

    Extracting headlines and summaries from HTML content using RegEx

--- regular_expressions.py
+++ regular_expressions.py
@@ -2,16 +2,29 @@
 For this homework, you will need to write a Python program 
 that reads the HTML content of a web page and extracts the 
 headlines and summaries of the articles on that page.
+
+Author: Rachel Newman
+
+Student ID: 2562294
+
+Resources: All of the code was copied from Canvas for this assignment. I finished the regex for the get_headlines function and the 
+get_summaries functions. When running the code, I realized my output didn't exactly match the one on Canvas. In an attempt to debug 
+my code, I used the reference websites posted in the module for Regular Expressions, and I attempted to copy my code to ChatGPT to
+figure out what I was doing wrong. ChatGPT gave me several different options for my regex in both functions, but they resulted in the 
+same exact output, so I just kept what I initially put.
 """
 import re
 from urllib.request import Request, urlopen
 
 """Okay, so I fear I have incorrectly done the regex for both functions because the output doesn't exactly match the output in the
-assignment instructions. I may be incorrectly pulling the headers because I  """
+assignment instructions. I may be incorrectly pulling the headers because the output I get is a string starting with '<a href ...</a>'
+for all 37 of the headers. For the summaries, all of them print out fine, but there is an extra summary that it is pulling for 
+some reason (hence the output having 38 instead of 37). I am horrible at debugging code because I don't understand a lot of code, even
+when I try to understand, it just flies over my head.  """
 
 def get_headlines(html: str) -> list[str]:
     """Extracts the headlines from the HTML content"""
-    h3_pattern = re.compile(r"<h3.*?>(.*?)</h3>", re.DOTALL)
+    h3_pattern = re.compile(r"<h3>(.*?)</h3>", re.DOTALL)
     return h3_pattern.findall(html)
 
 
